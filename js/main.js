9c611c18eb67f80a189b6957766b4fc00f2fdcab
geoplanet <jeff@geoprism.ca>
2016-03-23 16:50:47 -0700

    Remove bug tracking from home page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-var urlToGetAllOpenBugs = "https://api.github.com/repos/openbroadcaster/obplayer/issues?state=open";
+var urlToGetOpenPlayerBugs = "https://api.github.com/repos/openbroadcaster/obplayer/issues?state=open";
+var urlToGetOpenServerBugs = "https://api.github.com/repos/openbroadcaster/observer/issues?state=open";
+
 
 $(document).ready(function () {
-    $.getJSON(urlToGetAllOpenBugs, function (allIssues) {
-        if (allIssues.length ==0) $(bugs).append("No open issues</br>");
+    $.getJSON(urlToGetOpenPlayerBugs, function (allIssues) {
+        if (allIssues.length ==0) $(ply_bugs).append("No open issues</br>");
         $.each(allIssues, function (i, issue) {
-            $(bugs)
+            $(ply_bugs)
+                .append("<b>" + issue.number + " - " + issue.title + "</b></br>")
+//                .append("created at: " + issue.created_at + "</br>")
+//                .append(issue.body + "</br></br></br>");
+        });
+    });
+
+    $.getJSON(urlToGetOpenServerBugs, function (allIssues) {
+        if (allIssues.length ==0) $(srv_bugs).append("No open issues</br>");
+        $.each(allIssues, function (i, issue) {
+            $(srv_bugs)
                 .append("<b>" + issue.number + " - " + issue.title + "</b></br>")
 //                .append("created at: " + issue.created_at + "</br>")
 //                .append(issue.body + "</br></br></br>");
