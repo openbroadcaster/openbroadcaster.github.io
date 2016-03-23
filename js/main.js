66bc50f48759b287a6fd9a37b039ceb7d1eb265b
geoplanet <jeff@geoprism.ca>
2016-03-23 16:38:01 -0700

    Bug reporting and issue tracking!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-$(document).ready( function() {
+var urlToGetAllOpenBugs = "https://api.github.com/repos/openbroadcaster/obplayer/issues?state=open";
+
+$(document).ready(function () {
+    $.getJSON(urlToGetAllOpenBugs, function (allIssues) {
+        if (allIssues.length ==0) $(bugs).append("No open issues</br>");
+        $.each(allIssues, function (i, issue) {
+            $(bugs)
+                .append("<b>" + issue.number + " - " + issue.title + "</b></br>")
+//                .append("created at: " + issue.created_at + "</br>")
+//                .append(issue.body + "</br></br></br>");
+        });
+    });
     $('[data-toggle="tooltip"]').tooltip();
 });
+
