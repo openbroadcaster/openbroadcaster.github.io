eb29de967b004572d151b168aa8b29ff3b2f23d3
geoplanet <jeff@geoprism.ca>
2016-04-08 09:00:09 -0700

    open bugs only

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var urlToGetOpenPlayerBugs = "https://api.github.com/repos/openbroadcaster/obplayer/issues?state=all";
+var urlToGetOpenPlayerBugs = "https://api.github.com/repos/openbroadcaster/obplayer/issues?state=open";
 var urlToGetOpenServerBugs = "https://api.github.com/repos/openbroadcaster/observer/issues?state=open";
 
 
